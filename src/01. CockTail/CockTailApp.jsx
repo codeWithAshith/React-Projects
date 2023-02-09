d9888831417c10ff128b7e79bf22d431f055b03d
codewithashith <codewithashith@gmail.com>
2023-02-09 14:48:04 +0530

    02. Router setup

--- src/01. CockTail/CockTailApp.jsx
+++ src/01. CockTail/CockTailApp.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./index.css";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import SingleCocktail from "./pages/SingleCocktail";
+
+// npm i react-router@6
+// npm install react-router-dom --save
 
 const CockTailApp = () => {
-  return <div>CockTailApp</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cocktail/:id" element={<SingleCocktail />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default CockTailApp;
