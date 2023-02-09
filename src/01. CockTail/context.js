b7052d1f0a20e23cd3504cc68e54f1eeb136ab86
codewithashith <codewithashith@gmail.com>
2023-02-09 17:18:20 +0530

    05. Context and search term

--- src/01. CockTail/context.js
+++ src/01. CockTail/context.js
@@ -8,37 +8,9 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
-  const [cocktails, setCocktails] = useState([]);
 
   const fetchDrinks = useCallback(async () => {
     setLoading(true);
-    try {
-      const response = await fetch(`${url}${searchTerm}`);
-      const data = await response.json();
-      console.log(data);
-      const { drinks } = data;
-      if (drinks) {
-        const newCocktails = drinks.map((item) => {
-          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
-            item;
-
-          return {
-            id: idDrink,
-            name: strDrink,
-            image: strDrinkThumb,
-            info: strAlcoholic,
-            glass: strGlass,
-          };
-        });
-        setCocktails(newCocktails);
-      } else {
-        setCocktails([]);
-      }
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
   }, [searchTerm]);
 
   useEffect(() => {
@@ -46,9 +18,7 @@ const AppProvider = ({ children }) => {
   }, [searchTerm, fetchDrinks]);
 
   return (
-    <AppContext.Provider
-      value={{ loading, cocktails, searchTerm, setSearchTerm }}
-    >
+    <AppContext.Provider value={{ loading, searchTerm, setSearchTerm }}>
       {children}
     </AppContext.Provider>
   );
