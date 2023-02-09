0b758a6724b31c904feb0986d4e047f9fb297a58
codewithashith <codewithashith@gmail.com>
2023-02-09 17:22:34 +0530

    06. Fetch Drinks

--- src/01. CockTail/components/CocktailList.jsx
+++ src/01. CockTail/components/CocktailList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "../context";
+import Cocktail from "./Cocktail";
 import Loading from "./Loading";
 
 const CocktailList = () => {
@@ -20,7 +21,11 @@ const CocktailList = () => {
   return (
     <section className="section">
       <h2 className="section-title">cocktails</h2>
-      <h2>cocktail list component</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
     </section>
   );
 };
--- src/01. CockTail/context.js
+++ src/01. CockTail/context.js
@@ -8,9 +8,36 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
+  const [cocktails, setCocktails] = useState([]);
 
   const fetchDrinks = useCallback(async () => {
     setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      console.log(data);
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   }, [searchTerm]);
 
   useEffect(() => {
@@ -18,7 +45,9 @@ const AppProvider = ({ children }) => {
   }, [searchTerm, fetchDrinks]);
 
   return (
-    <AppContext.Provider value={{ loading, searchTerm, setSearchTerm }}>
+    <AppContext.Provider
+      value={{ loading, cocktails, searchTerm, setSearchTerm }}
+    >
       {children}
     </AppContext.Provider>
   );
